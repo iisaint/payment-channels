1e30cbfb8e2d9c11f5a6bd42c9f2b6656acbcbbd
Yao-Hsin Chen <yaohsin@itri.org.tw>
2018-08-27 23:07:13 +0800

    replace web3 0.x with web3 1.0

--- test/payment.test.js
+++ test/payment.test.js
@@ -1,21 +1,24 @@
-const Payment = artifacts.require("payment");
-const web3 = require('web3');
+const { getWeb3, getContractInstance} = require('./helpers/web3.10');
+const web3 = getWeb3();
+const getInstance = getContractInstance(web3);
 
 contract('Payment channel test', async (accounts) => {
 
   it("should open channel with 1 ether in the first account", async () => {
     const sender = accounts[0];
     const recipient = accounts[1];
-    let instance = await Payment.deployed();
+    // let instance = await Payment.deployed();
+    const instance = getInstance("Payment");
     let channel = instance;
     const timeout = 60*60*24;
     const value = web3.utils.toWei('1', 'ether');
-    await channel.openChannel(recipient, timeout, {from: sender, value: web3.utils.toWei('1', 'ether')});
-    const _sender = await channel.sender.call();
-    const _recipient = await channel.recipient.call();
-    const _timeout = await channel.timeout.call();
-    assert.equal(_sender, sender);
-    assert.equal(_recipient, recipient);
+    await channel.methods.openChannel(recipient, timeout).send({from: sender, value: web3.utils.toWei('1', 'ether')});
+    const _sender = await channel.methods.sender().call();
+    console.log(_sender);
+    const _recipient = await channel.methods.recipient().call();
+    const _timeout = await channel.methods.timeout().call();
+    assert.equal(_sender.toLowerCase(), sender);
+    assert.equal(_recipient.toLowerCase(), recipient);
     // assert.equal(web3.utils.hexToNumber(web3.utils.toHex(_timeout)), timeout);
   })
 })
