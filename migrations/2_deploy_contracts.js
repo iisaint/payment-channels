73ce452676935d9ae17c84b08550c8a7bca130ec
Yao-Hsin Chen <yaohsin@itri.org.tw>
2018-08-28 07:02:16 +0800

    clean up

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,7 @@
 const Payment = artifacts.require('Payment');
 
 module.exports = function(deployer, network) {
-  console.log(`network = ${network}`);
+  // console.log(`network = ${network}`);
   // deployment steps
   deployer.deploy(Payment);
 };
