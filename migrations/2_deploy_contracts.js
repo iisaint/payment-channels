ee71cbeb1e83c7140f52c530446735651f23ef90
Yao-Hsin Chen <yaohsin@itri.org.tw>
2018-08-27 21:40:48 +0800

    add network parameter

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,6 +1,7 @@
 const Payment = artifacts.require('Payment');
 
-module.exports = function(deployer) {
+module.exports = function(deployer, network) {
+  console.log(`network = ${network}`);
   // deployment steps
   deployer.deploy(Payment);
 };
